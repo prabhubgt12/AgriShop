7cd766f16ec932df8b77cd7231d7e03911598d08
prabhubgt12 <prabhubgt12@gmail.com>
2025-09-11 11:58:44 +0530

    version update

--- ledgerbook/build.gradle.kts
+++ ledgerbook/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.ledge.ledgerbook"
         minSdk = 24
         targetSdk = 35
-        versionCode = 10
-        versionName = "1.0.10"
+        versionCode = 11
+        versionName = "1.0.11"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables.useSupportLibrary = true
